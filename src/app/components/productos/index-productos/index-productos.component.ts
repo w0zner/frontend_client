2e05d3e1407a752685e9f4554e7188ab0ec60411
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2025-07-31 22:56:17 -0300

    ajuste obtener categorias

--- src/app/components/productos/index-productos/index-productos.component.ts
+++ src/app/components/productos/index-productos/index-productos.component.ts
@@ -11,8 +11,17 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class IndexProductosComponent implements OnInit {
 
   public config_global: any = {}
+  public someRange: number[] = [100, 700];
+  nombre_categoria: string = '';
 
   constructor(private usuarioService: UsuarioService) {
+    this.obtenerConfiguracionPublica();
+  }
+
+  ngOnInit(): void {
+  }
+
+  obtenerConfiguracionPublica() {
     this.usuarioService.obtenerConfiguracionPublica().subscribe({
       next: (response: any) => {
         this.config_global = response.data;
@@ -20,17 +29,21 @@ export class IndexProductosComponent implements OnInit {
       }
     })
   }
-
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  public someRange: number[] = [100, 700];
  
 
   changeSomeRange(index: number, value: number) {
     let newRange = [this.someRange[0], this.someRange[1]];
     newRange[index] = newRange[index] + value;
     this.someRange = newRange;
+  }
+
+  buscarCategorias() {
+    console.log(this.nombre_categoria);
+    if (this.nombre_categoria) {
+      const search = new RegExp(this.nombre_categoria, 'i');
+      this.config_global.categorias = this.config_global.categorias.filter((item:any) => search.test(item.titulo))
+    } else {
+      this.obtenerConfiguracionPublica();
+    }
   }
 }
