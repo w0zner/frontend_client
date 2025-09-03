2b69960b26fd25387f0eb8ee06c52c5c8400c0d5
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2025-09-02 22:27:49 -0300

    ajustes carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-carrito',
@@ -8,12 +11,35 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CarritoComponent implements OnInit {
 
+  userLogged: any = undefined
+  carrito: any[] = []
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private usuarioService: UsuarioService) {
 
   }
 
   ngOnInit(): void {
+    this.authService.usuario$.subscribe((usuario: any) => {
+      console.log(usuario)
+      if(usuario){
+        this.userLogged = usuario;
+
+        this.usuarioService.obtenerCarritoPorUsuario(usuario._id).subscribe({
+          next: (response:any) => {
+            console.log(response)
+            this.carrito = response.data;
+          }
+        });
+      }
+    });
+  }
+
+  calcularTotalCarrito() {
+    let total = 0;
+    this.carrito.forEach(item => {
+      total += item.producto.precio * item.cantidad
+    });
+    return total;
   }
 
 }
