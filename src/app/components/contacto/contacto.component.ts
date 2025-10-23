492e44aa813f9faa571e094d111b0c53a59f259f
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2025-10-22 23:14:17 -0300

    add contacto form

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  contactoForm: FormGroup
+
+  constructor(private fb: FormBuilder){
+    this.contactoForm = this.fb.group({
+      cliente: [''],
+      asunto: [''],
+      mensaje: [''],
+      telefono: [''],
+      correo: ['', [Validators.email]]
+    })
+  }
+
+  enviarContacto(){
+    if(this.contactoForm.valid){
+      console.log(this.contactoForm.value)
+    } else {
+      console.error('invalido')
+    }
+  }
 
 }
