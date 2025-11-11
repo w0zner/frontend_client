5b93893f026b12c7ecfbc3fa3082b04e7dab0bec
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2025-11-10 23:30:14 -0300

    avance en emision y actualización de resenha

--- src/app/components/usuario/ordenes/detalle-orden/detalle-orden.component.ts
+++ src/app/components/usuario/ordenes/detalle-orden/detalle-orden.component.ts
@@ -1,62 +1,116 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { GLOBAL } from 'src/app/services/GLOBAL';
-import { OrdenesService } from 'src/app/services/ordenes.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { GLOBAL } from "src/app/services/GLOBAL";
+import { OrdenesService } from "src/app/services/ordenes.service";
 
 @Component({
-  selector: 'app-detalle-orden',
-  templateUrl: './detalle-orden.component.html',
-  styleUrls: ['./detalle-orden.component.css']
+  selector: "app-detalle-orden",
+  templateUrl: "./detalle-orden.component.html",
+  styleUrls: ["./detalle-orden.component.css"],
 })
 export class DetalleOrdenComponent implements OnInit {
-
-  public id: string | null = null
-  detalles: any[] = []
+  public id: string | null = null;
+  detalles: any[] = [];
   orden: any;
   public urlProducto: string;
-  opcionResenha: string | undefined
-  reviewForm: FormGroup
+  opcionResenha: string | undefined;
+  reviewForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private acivatedRoute: ActivatedRoute, private ordenesservice: OrdenesService) {
-    this.urlProducto = GLOBAL.url + 'productos/obtenerPortada/'
+  constructor(
+    private fb: FormBuilder,
+    private acivatedRoute: ActivatedRoute,
+    private ordenesService: OrdenesService,
+  ) {
+    this.urlProducto = GLOBAL.url + "productos/obtenerPortada/";
     this.reviewForm = this.fb.group({
-      producto: [''],
-      usuario: [''],
-      venta: [''],
-      mensaje: [''],
-      opcion: ['']
-    })
+      producto: [""],
+      usuario: [""],
+      venta: [""],
+      mensaje: [""],
+      opcion: [""],
+    });
   }
 
   ngOnInit(): void {
-    this.acivatedRoute.paramMap.subscribe(params => {      
-      this.id= params.get('id');
-      if(this.id!=null){
-        this.ordenesservice.obtenerVentaPorId(this.id).subscribe({
+    this.acivatedRoute.paramMap.subscribe((params) => {
+      this.id = params.get("id");
+      if (this.id != null) {
+        this.ordenesService.obtenerVentaPorId(this.id).subscribe({
           next: (response: any) => {
-            
             this.orden = response.venta;
-            console.log('orde', this.orden)
+            console.log("orde", this.orden);
             this.detalles = response.detalles;
-            console.log('Detalle ', this.detalles)
-          }
-        })
+            console.log("Detalle ", this.detalles);
+          },
+        });
       }
-    })
+    });
+  }
+
+  setResenha(producto: any) {
+    this.ordenesService
+      .obtenerResenhaPorVentaProducto(this.orden._id, producto)
+      .subscribe({
+        next: (response: any) => {
+          console.info("Reseña ", response);
+          this.reviewForm.patchValue({
+            venta: this.orden._id,
+            usuario: this.orden.usuario._id,
+            producto: producto,
+            mensaje: response?.data[0]?.mensaje,
+            opcion: response?.data[0]?.opcion,
+          });
+          this.opcionResenha = response?.data[0]?.opcion;
+          console.log(this.reviewForm.value);
+
+          //VALIDACION DE FECHA PARA Reseña
+          // const fechaCreacion = new Date("2025-11-11T01:49:02.535Z");
+          //const ahora = new Date();
+
+          // Crear una copia de la fecha de creación y sumarle 1 mes
+          //const fechaMasUnMes = new Date(fechaCreacion);
+          //fechaMasUnMes.setMonth(fechaMasUnMes.getMonth() + 1);
+
+          // Comparar si la fecha actual es mayor o igual a un mes después
+          //const haPasadoUnMes = ahora >= fechaMasUnMes;
+
+          //console.log(haPasadoUnMes); // true o false
+          //
+          // O
+          //
+          //
+          // const fechaCreacion = new Date("2025-11-11T01:49:02.535Z");
+          //const ahora = new Date();
+
+          //const unMesEnMs = 30 * 24 * 60 * 60 * 1000; // 30 días aprox.
+          //const diferencia = ahora.getTime() - fechaCreacion.getTime();
+
+          //const haPasadoUnMes = diferencia >= unMesEnMs;
+
+          //console.log(haPasadoUnMes);
+        },
+      });
   }
 
   elegirResenha(opcion: string) {
-    this.opcionResenha = opcion
+    this.opcionResenha = opcion;
     this.reviewForm.patchValue({
-      opcion: opcion
-    })
+      opcion: opcion,
+    });
   }
 
-  emitirResenha(){
+  emitirResenha(producto: any) {
     this.reviewForm.patchValue({
-      
-    })
+      venta: this.orden._id,
+      usuario: this.orden.usuario._id,
+      producto: producto,
+    });
+    console.info(this.reviewForm.value);
+    this.ordenesService.guardarResenha(this.reviewForm.value).subscribe({
+      next: (response: any) => {
+        console.info(response);
+      },
+    });
   }
-
 }
--- src/app/services/ordenes.service.ts
+++ src/app/services/ordenes.service.ts
@@ -1,27 +1,50 @@
-import { Injectable } from '@angular/core';
-import { AuthService } from './auth.service';
-import { HttpClient } from '@angular/common/http';
-import { GLOBAL } from './GLOBAL';
+import { Injectable } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { HttpClient } from "@angular/common/http";
+import { GLOBAL } from "./GLOBAL";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class OrdenesService {
-
   private url: string;
-  private headers: any
+  private urlReview: string;
+  private headers: any;
 
-  constructor(private http: HttpClient, private authService: AuthService) {
-    this.url = GLOBAL.url + 'ventas';
-    this.headers = this.authService.getHeaders()
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) {
+    this.url = GLOBAL.url + "ventas";
+    this.urlReview = GLOBAL.url + "review";
+    this.headers = this.authService.getHeaders();
   }
 
-  obtenerListaOrdenes(usuario:any){
-    return this.http.get(this.url + '/obtener-ventas-usuario/' + usuario, {headers: this.headers})
+  obtenerListaOrdenes(usuario: any) {
+    return this.http.get(this.url + "/obtener-ventas-usuario/" + usuario, {
+      headers: this.headers,
+    });
   }
 
-  obtenerVentaPorId(id:any){
-    return this.http.get(this.url + '/obtener-venta/' + id, {headers: this.headers})
+  obtenerVentaPorId(id: any) {
+    return this.http.get(this.url + "/obtener-venta/" + id, {
+      headers: this.headers,
+    });
   }
-  
+
+  guardarResenha(data: any) {
+    return this.http.post(this.urlReview, data, { headers: this.headers });
+  }
+
+  obtenerResenhaPorVentaProducto(venta: any, producto: any) {
+    return this.http.get(this.urlReview + "/" + venta + "/" + producto, {
+      headers: this.headers,
+    });
+  }
+
+  actualizarResenha(id: any, data: any) {
+    return this.http.put(this.urlReview + "/" + id, data, {
+      headers: this.headers,
+    });
+  }
 }
